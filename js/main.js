0d9af729c67b7eb5d46fdbc4b9ea79e52169b2b4
maximsan <maximuzz@yandex.ru>
2020-09-10 01:09:04 +0300

    YCK-XX: do almost right js import setup

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
+import $ from 'jquery';
+import AOS from 'aos';
+import { closeSideBar, drawSidebar, toggleSideBar } from './sideBar';
+import { googleMapInit } from './googleMapInit';
+
 const items = $('.package > .package-list > .package-list-item'); // packages
 const commonSpan = `<span class="package-list-item-disc"></span>✔️</span>`;
 
 function commonMethods() {
     addFixedHeader();
     toggleSideBar();
-    redrawSidebar();
+    drawSidebar();
     hideElementOnClickOutside();
     closeSideBar();
 }
 
 $(function() {
+    googleMapInit();
+
     if ($(window).width() >= 768) {
         addFixedHeaderOnScroll();
         smoothScroll(1500);
@@ -26,56 +33,6 @@ $(function() {
         redrawPackagesForSmallSmartphones();
     }
 });
-
-function toggleSideBar() {
-    $('.navbar-toggler').click((e) => {
-        e.preventDefault();
-        $('.sidebar').toggleClass('open');
-        $('.navbar-toggler-icon').toggleClass('hidden');
-        $('.close-icon').toggleClass('hidden');
-    });
-}
-
-function redrawSidebar() {
-    const house = `<span class="house-order">Заказать 🏠 </span>`;
-    const phones = `<a class="phone text-white decoration-none"
-    href="tel:+375295695989">
-    <span>+375 (29) 569 59 89</span></a>
-    <a class="phone text-white decoration-none"
-    href="tel:+375291495989">
-    <span>+375 (29) 149 59 89</span></a>`;
-
-    var navbarChildren = $('.navbar-nav li');
-    var firstElem = navbarChildren.first();
-    firstElem.children()[0].innerHTML = house;
-    firstElem.children()[2].innerHTML = phones;
-
-    $('.navbar-nav li')
-        .first()
-        .remove();
-
-    $('.navbar-nav').append(firstElem);
-
-    $('.navbar-nav').append(`<li class="nav-item">
-  <a class="nav-link" href="#map">
-    Как к нам добраться?
-  </a></li>`);
-}
-
-function closeSideBar() {
-    /* close sidebar after clicking on menu point */
-    $(
-            `
-            a[href$='services'],
-            a[href$='prices'],
-            a[href$='contacts'],
-            a[href$='map'],
-            a[href$='packages']
-        `
-    ).click((e) => {
-        setTimeout(() => $('.sidebar').removeClass('open'), 0);
-    });
-}
 
 function hideElementOnClickOutside(element = '.sidebar', menuWidth = 260) {
     $(document).click((e) => {
@@ -175,8 +132,9 @@ function addFixedHeader() {
 }
 
 function addFixedHeaderOnScroll() {
-    var $fixedElement = $('.header');
-    $clone = $fixedElement.before($fixedElement.clone().addClass('fixed'));
+    const fixedElement = $('.header');
+    fixedElement.before(fixedElement.clone().addClass('fixed'));
+
     $(window).scroll(function() {
         var fromTop = $(window).scrollTop();
         $('body').toggleClass('down', fromTop > 480);
@@ -220,190 +178,13 @@ function smoothScroll(milliseconds) {
     return false;
 }
 
-// Change lat and lng
-function googleMapInit() {
-    var lat = {
-        lat: 54.291195,
-        lng: 27.477718
-    };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: lat,
-        mapTypeId: 'satellite',
-        styles: [
-            {
-                elementType: 'geometry',
-                stylers: [
-                    {
-                        color: '#f5f5f5'
-                    }
-                ]
-            },
-            {
-                elementType: 'labels.icon',
-                stylers: [
-                    {
-                        visibility: 'off'
-                    }
-                ]
-            },
-            {
-                elementType: 'labels.text.fill',
-                stylers: [
-                    {
-                        color: '#616161'
-                    }
-                ]
-            },
-            {
-                elementType: 'labels.text.stroke',
-                stylers: [
-                    {
-                        color: '#f5f5f5'
-                    }
-                ]
-            },
-            {
-                featureType: 'administrative.land_parcel',
-                elementType: 'labels.text.fill',
-                stylers: [
-                    {
-                        color: '#bdbdbd'
-                    }
-                ]
-            },
-            {
-                featureType: 'poi',
-                elementType: 'geometry',
-                stylers: [
-                    {
-                        color: '#eeeeee'
-                    }
-                ]
-            },
-            {
-                featureType: 'poi',
-                elementType: 'labels.text.fill',
-                stylers: [
-                    {
-                        color: '#757575'
-                    }
-                ]
-            },
-            {
-                featureType: 'poi.park',
-                elementType: 'geometry',
-                stylers: [
-                    {
-                        color: '#e5e5e5'
-                    }
-                ]
-            },
-            {
-                featureType: 'poi.park',
-                elementType: 'labels.text.fill',
-                stylers: [
-                    {
-                        color: '#9e9e9e'
-                    }
-                ]
-            },
-            {
-                featureType: 'road',
-                elementType: 'geometry',
-                stylers: [
-                    {
-                        color: '#ffffff'
-                    }
-                ]
-            },
-            {
-                featureType: 'road.arterial',
-                elementType: 'labels.text.fill',
-                stylers: [
-                    {
-                        color: '#757575'
-                    }
-                ]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'geometry',
-                stylers: [
-                    {
-                        color: '#dadada'
-                    }
-                ]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'labels.text.fill',
-                stylers: [
-                    {
-                        color: '#616161'
-                    }
-                ]
-            },
-            {
-                featureType: 'road.local',
-                elementType: 'labels.text.fill',
-                stylers: [
-                    {
-                        color: '#9e9e9e'
-                    }
-                ]
-            },
-            {
-                featureType: 'transit.line',
-                elementType: 'geometry',
-                stylers: [
-                    {
-                        color: '#e5e5e5'
-                    }
-                ]
-            },
-            {
-                featureType: 'transit.station',
-                elementType: 'geometry',
-                stylers: [
-                    {
-                        color: '#eeeeee'
-                    }
-                ]
-            },
-            {
-                featureType: 'water',
-                elementType: 'geometry',
-                stylers: [
-                    {
-                        color: '#c9c9c9'
-                    }
-                ]
-            },
-            {
-                featureType: 'water',
-                elementType: 'labels.text.fill',
-                stylers: [
-                    {
-                        color: '#9e9e9e'
-                    }
-                ]
-            }
-        ],
-        scrollwheel: false
-    });
-    var marker = new google.maps.Marker({
-        position: lat,
-        map: map
-    });
-}
-
 const modal = document.querySelector('#exampleModalCenter');
 const contactForm = document.querySelector('.contact-form');
 const name = contactForm.querySelector('#name');
 const email = contactForm.querySelector('#email');
 const phone = contactForm.querySelector('#phone');
 const message = contactForm.querySelector('#message');
+
 // contactForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
@@ -418,10 +199,19 @@ function handleSubmit(event) {
         body
     })).then((result) => {
         console.log(result);
-        $('#exampleModalCenter').modal('hide');
+        $(modal).modal('hide');
     }).catch((error) => {
         console.error(error.message);
     }).finally(() => {
-        $('#exampleModalCenter').modal('hide');
+        $(modal).modal('hide');
     });
 }
+
+$('.carousel').carousel({
+    interval: 4000
+});
+
+AOS.init({
+    disable: 'mobile'
+});
+AOS.refresh();
