578b15a394787c9eb44a4fc5fdbdaf92dabb2186
maximsan <maximuzz@yandex.ru>
2020-09-12 17:37:38 +0300

    YCK-XX: rename images and add loading with webpack

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
  * Application entry point
  */
 
+// load styles
+
 //Bootstrap core CSS
 import 'css/normalize.css';
 import 'css/bootstrap.css';
@@ -11,9 +13,29 @@ import 'css/main.css';
 import 'scss/main.scss';
 
 //load images
-//load other js files
+import '../assets/images/icons/airbnb.svg';
+import '../assets/images/icons/booking.svg';
+import '../assets/images/icons/mail.svg';
+import '../assets/images/icons/mts.svg';
+import '../assets/images/icons/velcom.svg';
+import '../assets/images/carousel/view_0_1900x1080.jpg';
+import '../assets/images/carousel/view_1_1900x1080.jpg';
+import '../assets/images/carousel/view_2_1900x1080.jpg';
+import '../assets/images/carousel/view_3_1900x1080.jpg';
+import '../assets/images/bathhouse/bad_510x400.jpg';
+import '../assets/images/bathhouse/bath_510x400.jpg';
+import '../assets/images/bathhouse/bathhouse_510x400.jpg';
+import '../assets/images/bathhouse/shower_510x400.jpg';
+import '../assets/images/fishing/dam_800x400.jpg';
+import '../assets/images/fishing/fish_800x400.jpg';
+import '../assets/images/fishing/pier_800x400.jpg';
+import '../assets/images/fishing/view_with_houses_800x400.jpg';
+import '../assets/icons/apple-touch-icon.png';
+import '../assets/icons/favicon-16x16.png';
+import '../assets/icons/favicon-32x32.png';
+
+//load js files
 import './vendor/modernizr-3.11.2.min';
 import 'bootstrap/dist/js/bootstrap.min';
-
 import './plugins';
 import './main';
