8266f174abb2ef84a69b4866e61fcc501774f21a
maksimshchutski <maksim.shchutski@itrexgroup.com>
2023-04-16 13:48:40 +0300

    disable scrolling when sidebar opens

--- src/js/helpers/sideBar.js
+++ src/js/helpers/sideBar.js
@@ -2,20 +2,31 @@ import $ from 'jquery';
 
 const sideBar = $('.sidebar');
 const navBarToggleBtn = $('.navbar-toggler');
+const togglerIcon = $('.navbar-toggler-icon');
 
 function openSideBar() {
     sideBar.toggleClass('open');
+    document.body.style.overflow = 'hidden';
+    document.body.style.height = '100vh';
 }
 
 function closeSideBar() {
     sideBar.removeClass('open');
+    document.body.style.overflow = null;
+    document.body.style.height = null;
 }
 
 export function toggleSideBar() {
     navBarToggleBtn.click((e) => {
         e.preventDefault();
-        openSideBar();
-        $('.navbar-toggler-icon').toggleClass('hidden');
+
+        if(!togglerIcon.hasClass('hidden')) {
+            openSideBar();
+        } else {
+            closeSideBar();
+        }
+
+        togglerIcon.toggleClass('hidden');
         $('.close-icon').toggleClass('hidden');
     });
 }
