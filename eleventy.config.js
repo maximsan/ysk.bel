7d8ab59914323ca3e74de86c5ccc63d532be936f
maksimshchutski <maksim.shchutski@itrexgroup.com>
2023-06-26 00:20:29 +0200

    migration part-1: setup template

---
+++ eleventy.config.js
@@ -0,0 +1,35 @@
+
+module.exports = function (eleventyConfig) {
+    // Copy the contents of the `public` folder to the output folder
+    // For example, `./public/css/` ends up in `_site/css/`
+    eleventyConfig.addPassthroughCopy({
+        "./public/": "/",
+        "./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
+    });
+
+    // let ejs = require("ejs");
+    // eleventyConfig.setLibrary("ejs", ejs);
+
+    return {
+        templateFormats: [
+            "md",
+            "html",
+            "ejs"
+        ],
+
+        // Pre-process *.md files with: (default: `liquid`)
+        markdownTemplateEngine: "ejs",
+
+        // Pre-process *.html files with: (default: `liquid`)
+        htmlTemplateEngine: "ejs",
+
+        // These are all optional:
+        dir: {
+            input: "src",          // default: "."
+            output: 'dist',
+            // includes: 'includes',
+            // layouts: 'layouts',
+            // data: 'data'
+        }
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,30 @@
+/**
+ * Application entry point
+ */
+
+// load styles
+
+//Bootstrap core CSS
+import '../public/styles/css/normalize.css';
+import '../public/styles/css/bootstrap.css';
+import '../public/styles/css/main.css';
+
+//Custom styles
+import '../public/styles/scss/main.scss';
+import '../public/styles/scss/photoswipe/photo-swipe.scss';
+import '../public/styles/scss/photoswipe/default-skin.scss';
+
+//load images
+import '../public/assets/images/icons/mail.svg';
+import '../public/assets/images/icons/mts.svg';
+import '../public/assets/images/icons/a1.svg';
+import '../public/assets/images/icons/send-mail.svg';
+import '../public/assets/icons/apple-touch-icon.png';
+import '../public/assets/icons/favicon-16x16.png';
+import '../public/assets/icons/favicon-32x32.png';
+
+//load js files
+import './vendor/modernizr-3.11.2.min';
+import 'bootstrap/dist/js/bootstrap.min';
+import './helpers/plugins';
+import './main';
