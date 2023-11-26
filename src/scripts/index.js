d19dfba31da2fb0fe5eb7c6b75640b600ab699b1
maksimshchutski <maksim.shchutski@itrexgroup.com>
2023-11-26 23:18:16 +0100

    migration part-5: clean up

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,3 @@
-//load images
-// import '../assets/images/icons/mail.svg';
-// import '../assets/images/icons/mts.svg';
-// import '../assets/images/icons/a1.svg';
-// import '../assets/images/icons/send-mail.svg';
-// import '../assets/icons/apple-touch-icon.png';
-// import '../assets/icons/favicon-16x16.png';
-// import '../assets/icons/favicon-32x32.png';
-
-//load js files
 import 'bootstrap';
 import './helpers/plugins';
 import './main';
