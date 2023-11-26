a9b7bc08e83578e5eac7a24d7db5cb6dc281e886
maksimshchutski <maksim.shchutski@itrexgroup.com>
2023-11-26 23:14:17 +0100

    migration part-5: upd vendors css imports, location

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,3 @@
-/**
- * Application entry point
- */
-
-// load styles
-
-//Bootstrap core CSS
-// import '../styles/vendors/normalize.css';
-// import '../styles/vendors/bootstrap.css';
-// import '../styles/vendors/main.css';
-
-//Custom styles
-// import '../styles/main.scss';
-// import '../styles/photoswipe/photo-swipe.scss';
-// import '../styles/photoswipe/default-skin.scss';
-
 //load images
 // import '../assets/images/icons/mail.svg';
 // import '../assets/images/icons/mts.svg';
