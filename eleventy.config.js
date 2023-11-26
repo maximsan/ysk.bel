acbdcb709af5e5d83d30c856d27e63f870f66e25
maksimshchutski <maksim.shchutski@itrexgroup.com>
2023-11-26 22:53:50 +0100

    migration part-4: add esbuild, change scss folder, remove css completely, update js folders, add js setup

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,9 @@
 const sass = require("sass");
 const path = require("node:path");
 const { createHash } = require('node:crypto');
+const esbuild = require('esbuild');
 
-/*
-	For the given `content` string,
-	generate an MD5 hash of `length` chars.
- */
+/* For the given `content` string, generate an MD5 hash of `length` chars. */
 function getHash(content, length = 8) {
     return createHash('md5')
         .update(content)
@@ -13,90 +11,93 @@ function getHash(content, length = 8) {
         .substr(0, length);
 }
 
-module.exports = function (eleventyConfig) {
+module.exports = function (config) {
     // Copy the contents of the `public` folder to the output folder
     // For example, `./public/css/` ends up in `_site/css/`
-    eleventyConfig.addPassthroughCopy({
+    config.addPassthroughCopy({
         "src/assets/public": "/",
         // "./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
     });
-    eleventyConfig.addPassthroughCopy("src/assets/images")
-    eleventyConfig.addPassthroughCopy("src/assets/icons")
-    // eleventyConfig.addPassthroughCopy("src/styles/css")
+    config.addPassthroughCopy("src/assets/images")
+    config.addPassthroughCopy("src/assets/icons")
 
-    /*
-    Watch for changes in .scss files.
- */
-    eleventyConfig.addTemplateFormats('scss');
+    // css
+    /* Watch for changes in .scss files. */
+    config.addTemplateFormats('scss');
 
-    /*
-        Define how to process .scss files.
-     */
-    eleventyConfig.addExtension('scss', {
-        /*
-            We're feeding the `inputPath` to Sass directly, so we don't need Eleventy to read the content of `.scss` files.
-         */
+    /* Define how to process .scss files. */
+    config.addExtension('scss', {
+        outputFileExtension: 'css',
+        /* We're feeding the `inputPath` to sass directly, so we don't need Eleventy to read the content of `.scss` files. */
         read: false,
-
-        /*
-            Produce the data for each `.scss` file, including its processed CSS content and its MD5 content hash.
-         */
+        /* Produce the data for each `.scss` file, including its processed CSS content and its MD5 content hash. */
         getData: async function (inputPath) {
-            /*
-                Don't process .scss files that start with an underscore as standalone.
-             */
+            /* Don't process .scss files that start with an underscore as standalone. */
             if (path.basename(inputPath).startsWith('_')) {
                 return false;
             }
-            const { css } = sass.compile(inputPath);
+            const { css, loadedUrls, sourceMap } = sass.compile(inputPath);
             return {
-                /*
-                    Exclude .scss files from `collections.all` so they don't show up in sitemaps, RSS feeds, etc.
-                 */
+                /* Exclude .scss files from `collections.all` so they don't show up in sitemaps, RSS feeds, etc. */
                 eleventyExcludeFromCollections: true,
                 _content: css,
                 _hash: getHash(css)
             };
         },
         compileOptions: {
-            /*
-                Disable caching of `.scss` files, for good measure.
-            */
+            /* Disable caching of `.scss` files, for good measure. */
             cache: false,
             permalink: function (permalink, inputPath) {
-                /*
-                    Don't output .scss files that start with an underscore, as per Sass conventions…
-                 */
+                /* Don't output .scss files that start with an underscore, as per Sass conventions… */
                 if (path.basename(inputPath).startsWith('_')) {
                     return false;
                 }
-
-                /*
-                    …and for other .scss files include the MD5 content hash produced in the `.getData()` method in the output file path.
-                 */
+                /* …and for other .scss files include the MD5 content hash produced in the `.getData()` method in the output file path. */
                 return data => `${data.page.filePathStem}.${data._hash}.css`;
             }
         },
-        /*
-            Read the processed CSS content from the data object produced with `.getData()`.
-         */
+        /* Read the processed CSS content from the data object produced with `.getData()`. */
         compile: () => data => {
             return data._content
         }
     });
 
     const outputMap = {};
-    eleventyConfig.addTransform('outputMap', function (content) {
+    config.addTransform('outputMap', function (content) {
         const filepath = path.relative('src', this.page.inputPath);
         outputMap[filepath] = this.page.url;
         return content;
     });
 
-    eleventyConfig.addFilter('hashed', function (filepath) {
+    config.addFilter('hashed', function (filepath) {
         if (!outputMap[filepath]) {
             throw new Error(`hashed: ${filepath} not found in map.`);
         }
         return outputMap[filepath];
+    });
+
+    // JavaScript
+    config.addTemplateFormats('js');
+
+    config.addExtension('js', {
+        outputFileExtension: 'js',
+        compile: async (content, path) => {
+            if (path !== './src/scripts/index.js') {
+                return;
+            }
+
+            return async () => {
+                let output = await esbuild.build({
+                    target: 'es2020',
+                    entryPoints: [path],
+                    minify: true,
+                    bundle: true,
+                    write: false,
+                });
+
+                return output.outputFiles[0].text;
+            }
+        }
     });
 
     return {
@@ -106,10 +107,8 @@ module.exports = function (eleventyConfig) {
             "ejs",
             "njk"
         ],
-
-        // These are all optional:
         dir: {
-            input: "src",          // default: "."
+            input: "src",
             output: 'dist',
         }
     }
---
+++ src/scripts/index.js
@@ -0,0 +1,30 @@
+/**
+ * Application entry point
+ */
+
+// load styles
+
+//Bootstrap core CSS
+// import '../styles/vendors/normalize.css';
+// import '../styles/vendors/bootstrap.css';
+// import '../styles/vendors/main.css';
+
+//Custom styles
+// import '../styles/main.scss';
+// import '../styles/photoswipe/photo-swipe.scss';
+// import '../styles/photoswipe/default-skin.scss';
+
+//load images
+// import '../assets/images/icons/mail.svg';
+// import '../assets/images/icons/mts.svg';
+// import '../assets/images/icons/a1.svg';
+// import '../assets/images/icons/send-mail.svg';
+// import '../assets/icons/apple-touch-icon.png';
+// import '../assets/icons/favicon-16x16.png';
+// import '../assets/icons/favicon-32x32.png';
+
+//load js files
+import './vendor/modernizr-3.11.2.min';
+import 'bootstrap/dist/js/bootstrap.min';
+import './helpers/plugins';
+import './main';
