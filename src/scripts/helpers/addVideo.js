10b41ef76bcbe3d010813be763dfbc75fefd9492
maksimshchutski <maksim.shchutski@itrexgroup.com>
2024-02-04 21:11:22 +0100

    chore: remove redundant mixins, update video section js code

--- src/scripts/helpers/addVideo.js
+++ src/scripts/helpers/addVideo.js
@@ -1,6 +1,14 @@
 export const addVideo = () => {
     console.log('*** init video ***');
 
+    const match = window.matchMedia('(min-width: 768px)');
+    const videoSection = document.querySelector('.video-section');
+
+    if(!match.matches) {
+        videoSection.style.display = 'none';
+        return;
+    }
+
     const video = document.createElement('video');
     const source = document.createElement('source');
     const noJs = document.createElement('div');
@@ -13,8 +21,6 @@ export const addVideo = () => {
     source.src = 'assets/videos/main-video.mp4';
     source.type = 'video/mp4'
     noJs.innerHTML = 'Your browser does not support the video tag.'
-
-    const videoSection = document.querySelector('.video-section');
 
     video.appendChild(source);
     video.appendChild(noJs);
