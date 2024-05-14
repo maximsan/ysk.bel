af6a729792a2f83e5611ae98c7143de486a50cac
maksimshchutski <maksim.shchutski@itrexgroup.com>
2024-05-14 16:03:50 +0200

    update links

--- src/data/footer.js
+++ src/data/footer.js
@@ -1,18 +1,22 @@
+const PHONE_WITHOUT_PLUS = '375291495989';
+const PHONE_WITH_PLUS = `%2B${PHONE_WITHOUT_PLUS}`;
+const USER_NAME = 'oleg_miskevich';
+
 module.exports = {
     links: [
         {
             class: 'viber',
-            href: 'viber://chat?number=%2B375291495989',
+            href: `viber://chat/?number=${PHONE_WITH_PLUS}`,
             text: 'Viber',
         },
         {
             class: 'whatsapp',
-            href: 'https://api.whatsapp.com/send?phone=375291495989',
+            href: `https://api.whatsapp.com/send?phone=${PHONE_WITHOUT_PLUS}`,
             text: 'WhatsApp',
         },
         {
             class: 'skype',
-            href: 'skype:oleg_miskevich?chat',
+            href: `skype:${USER_NAME}?chat`,
             text: 'Skype',
         },
     ],
