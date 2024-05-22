88d127f1304fec97d39432166b9a8cea89479776
maksimshchutski <maksim.shchutski@itrexgroup.com>
2024-05-22 22:54:15 +0300

    add deploy to custom hosting

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Настройки
+# REMOTE_USER=maximsan
+# REMOTE_HOST=45.87.219.5
+# REMOTE_PATH=/httpdocs
+# LOCAL_PATH=/dist
+
+echo "Reading variables from .env file..."
+
+if [ -f .env ]; then
+  export $(cat .env | xargs)
+fi
+
+echo "Starting deploy..."
+
+# Команда для синхронизации файлов
+rsync -avz --delete $LOCAL_PATH $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+
+# Выполнение дополнительных команд на сервере (если необходимо)
+ssh -t $REMOTE_USER@$REMOTE_HOST << EOF
+  cd $REMOTE_PATH || exit 1
+  # Здесь можно выполнить команды, например, перезапуск сервера
+  # ./restart_server.sh
+EOF
+
+echo "Deployment completed!"
